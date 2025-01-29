d1fc41ed109f9945c8845def45229ea6bcb228f2
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2025-01-29 10:50:24 -0400

    fix: Improve texts, fix tests, and update images (#3)

---
+++ .envs/.env.tpl
@@ -0,0 +1,88 @@
+# SUGAR
+SUGAR_GROUP=${SUGAR_GROUP}
+
+# PostgreSQL
+# ------------------------------------------------------------------------------
+POSTGRES_HOST=${POSTGRES_HOST}
+POSTGRES_PORT=${POSTGRES_PORT}
+POSTGRES_DB=${POSTGRES_DB}
+POSTGRES_USER=${POSTGRES_USER}
+POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
+
+# DJANGO
+DATABASE_URL=${DATABASE_URL}
+# General
+# ------------------------------------------------------------------------------
+USE_DOCKER=${USE_DOCKER}
+IPYTHONDIR=${IPYTHONDIR}
+# Redis
+# ------------------------------------------------------------------------------
+REDIS_URL=${REDIS_URL}
+
+# Celery
+# ------------------------------------------------------------------------------
+
+# Flower
+CELERY_FLOWER_USER=${CELERY_FLOWER_USER}
+CELERY_FLOWER_PASSWORD=${CELERY_FLOWER_PASSWORD}
+CELERY_BROKER_URL=${CELERY_BROKER_URL}
+CELERY_FLOWER_PORT=${CELERY_FLOWER_PORT}
+
+
+# General
+# ------------------------------------------------------------------------------
+# DJANGO_READ_DOT_ENV_FILE=
+DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE}
+DJANGO_SECRET_KEY=${DJANGO_SECRET_KEY}
+DJANGO_ADMIN_URL=${DJANGO_ADMIN_URL}
+DJANGO_ALLOWED_HOSTS=${DJANGO_ALLOWED_HOSTS}
+DJANGO_STATIC_ROOT=${DJANGO_STATIC_ROOT}
+
+# Security
+# ------------------------------------------------------------------------------
+# TIP: better off using DNS, however, redirect is OK too
+DJANGO_SECURE_SSL_REDIRECT=${DJANGO_SECURE_SSL_REDIRECT}
+
+# Email
+# ------------------------------------------------------------------------------
+DJANGO_SERVER_EMAIL=${DJANGO_SERVER_EMAIL}
+EMAIL_BACKEND=${EMAIL_BACKEND}
+EMAIL_HOST=${EMAIL_HOST}
+EMAIL_HOST_USER=${EMAIL_HOST_USER}
+EMAIL_HOST_PASSWORD=${EMAIL_HOST_PASSWORD}
+EMAIL_PORT=${EMAIL_PORT}
+EMAIL_SENDER=${EMAIL_SENDER}
+EMAIL_USE_TLS=${EMAIL_USE_TLS}
+EMAIL_USE_SSL=${EMAIL_USE_SSL}
+
+
+# django-allauth
+# ------------------------------------------------------------------------------
+DJANGO_ACCOUNT_ALLOW_REGISTRATION=${DJANGO_ACCOUNT_ALLOW_REGISTRATION}
+
+# Gunicorn
+# ------------------------------------------------------------------------------
+WEB_CONCURRENCY=${WEB_CONCURRENCY}
+
+# Sentry
+# ------------------------------------------------------------------------------
+SENTRY_DSN=${SENTRY_DSN}
+
+
+# Redis
+# ------------------------------------------------------------------------------
+REDIS_URL=${REDIS_URL}
+
+# Celery
+# ------------------------------------------------------------------------------
+
+# Flower
+CELERY_FLOWER_USER=${CELERY_FLOWER_USER}
+CELERY_FLOWER_PASSWORD=${CELERY_FLOWER_PASSWORD}
+
+# Nginx
+CERTBOT_DOMAIN=${CERTBOT_DOMAIN}
+CERTBOT_EMAIL=${CERTBOT_EMAIL}
+
+# DOCKER
+DOCKER_BUILDKIT=0
--- src/backend/conftest.py
+++ src/backend/conftest.py
@@ -1,15 +1,10 @@
 """Test configuration module."""
 
-from pathlib import Path
-
 import pytest
-import yaml
 
-from ai_profile.models import AIProfile
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 from seconddx_web.users.models import User
-from user_profile.models import UserProfile
 
 
 @pytest.fixture
@@ -19,100 +14,6 @@ def user(db) -> User:
         email="test@seconddx.com",
         password="password",  # noqa: S106
     )
-
-
-@pytest.fixture
-def ai_profile(db, user) -> AIProfile:
-    """Return a AI profile as fixture."""
-    profile_path = (
-        Path(__file__).parent
-        / "ai_profile"
-        / "tests"
-        / "data"
-        / "ai_profile.yaml"
-    )
-
-    with Path.open(profile_path) as f:
-        profile_data = yaml.safe_load(f)
-
-    gender = (
-        "M"
-        if profile_data["gender"] == "male"
-        else "F"
-        if profile_data["gender"] == "female"
-        else "O"
-    )
-
-    profiles = AIProfile.objects.filter(user=user)
-
-    profiles.update(
-        user=user,
-        name=profile_data["name"],
-        age=profile_data["age"],
-        gender=gender,
-        interests=profile_data["interests"],
-        emotions=profile_data["emotions"],
-        bio_life=profile_data["bio"]["life"],
-        bio_education=profile_data["bio"]["education"],
-        bio_work=profile_data["bio"]["work"],
-        bio_family=profile_data["bio"]["family"],
-        bio_friends=profile_data["bio"]["friends"],
-        bio_pets=profile_data["bio"]["pets"],
-        bio_health=profile_data["bio"]["health"],
-    )
-
-    profile = profiles.first()
-
-    if not profile:
-        raise Exception("No ai user profile available for given user.")
-
-    return profile
-
-
-@pytest.fixture
-def user_profile(db, user) -> UserProfile:
-    """Return a user profile as fixture."""
-    profile_path = (
-        Path(__file__).parent
-        / "user_profile"
-        / "tests"
-        / "data"
-        / "user_profile.yaml"
-    )
-
-    with Path.open(profile_path) as f:
-        profile_data = yaml.safe_load(f)
-
-    gender = (
-        "M"
-        if profile_data["gender"] == "male"
-        else "F"
-        if profile_data["gender"] == "female"
-        else "O"
-    )
-
-    profiles = UserProfile.objects.filter(user=user)
-
-    profiles.update(
-        name=profile_data["name"],
-        age=profile_data["age"],
-        gender=gender,
-        interests=profile_data["interests"],
-        emotions=profile_data["emotions"],
-        bio_life=profile_data["bio"]["life"],
-        bio_education=profile_data["bio"]["education"],
-        bio_work=profile_data["bio"]["work"],
-        bio_family=profile_data["bio"]["family"],
-        bio_friends=profile_data["bio"]["friends"],
-        bio_pets=profile_data["bio"]["pets"],
-        bio_health=profile_data["bio"]["health"],
-    )
-    profile = profiles.first()
-
-    if not profile:
-        raise Exception("No user profile available for given user.")
-
-    return profile
 
 
 @pytest.fixture
