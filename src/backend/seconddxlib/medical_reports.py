94a4060b4118ca2c4ca1180bfd8f1f74c1535b9a
Sandro Loch <es.loch@gmail.com>
2025-03-04 16:17:33 -0300

    feat: Create 'medical_reports' App

---
+++ src/backend/seconddxlib/medical_reports.py
@@ -0,0 +1,13 @@
+""""""
+""""""
+
+# from rago.retrieval.tools import is_pdf, extract_text_from_pdf
+
+# def extract_content_from_pdf(pdf_file: str):
+#     if is_pdf(pdf_file):
+#         return extract_text_from_pdf(pdf_file)
+
+def extract_content_from_pdf(pdf_file: str):
+    
+
+    return pdf_file
