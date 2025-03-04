a7ae341bacd1b00a3ab0ad4ca4f5525f7a9a184e
Sandro Loch <es.loch@gmail.com>
2025-03-04 16:21:00 -0300

    chore(linting): Fix mypy issues

--- src/backend/seconddxlib/medical_reports.py
+++ src/backend/seconddxlib/medical_reports.py
@@ -1,13 +1,5 @@
 """"""
-""""""
 
-# from rago.retrieval.tools import is_pdf, extract_text_from_pdf
-
-# def extract_content_from_pdf(pdf_file: str):
-#     if is_pdf(pdf_file):
-#         return extract_text_from_pdf(pdf_file)
 
 def extract_content_from_pdf(pdf_file: str):
-    
-
     return pdf_file
--- src/backend/seconddxlib/tests/test_medical_report.py
+++ src/backend/seconddxlib/tests/test_medical_report.py
@@ -1,11 +1,7 @@
-
-import pytest
-from seconddxlib.medical_reports import extract_content_from_pdf 
+from seconddxlib.medical_reports import extract_content_from_pdf
 
 
 def test_extract_content_from_pdf():
+    pdf_file2 = extract_content_from_pdf("opened")
 
-    pdf_file2 = extract_content_from_pdf("opened") 
-
-    assert  pdf_file2 == "opened"
-    
+    assert pdf_file2 == "opened"
