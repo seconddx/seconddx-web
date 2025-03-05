330176610603dd7fba67957bf1ee07f9297dd053
Sandro Loch <es.loch@gmail.com>
2025-03-04 22:27:07 -0300

    chore(deps): fix PyTorch install error in RAGO dependencies

--- src/backend/seconddxlib/medical_reports.py
+++ src/backend/seconddxlib/medical_reports.py
@@ -1,5 +1,11 @@
-""""""
+from __future__ import annotations
+
+from rago.retrieval.tools.pdf import extract_text_from_pdf, is_pdf
 
 
-def extract_content_from_pdf(pdf_file: str):
-    return pdf_file
+def extract_content_from_pdf(file_path: str) -> str:
+    """Extract text content from a medical PDF file."""
+    if is_pdf(file_path):
+        return extract_text_from_pdf(file_path)
+
+    return ""
--- src/backend/seconddxlib/tests/test_medical_report.py
+++ src/backend/seconddxlib/tests/test_medical_report.py
@@ -1,7 +1,19 @@
+from pathlib import Path
+
+import pytest
+
 from seconddxlib.medical_reports import extract_content_from_pdf
 
 
-def test_extract_content_from_pdf():
-    pdf_file2 = extract_content_from_pdf("opened")
+@pytest.fixture
+def file_path() -> Path:
+    """Fixture to provide the path to a sample medical report PDF."""
+    return Path(__file__).parent / "data" / "Lab_Test_Menu_rev_Oct2019.pdf"
 
-    assert pdf_file2 == "opened"
+
+def test_extract_content_from_pdf(file_path: Path) -> None:
+    """Test extracting text from a medical report PDF."""
+    pdf_text: str = extract_content_from_pdf(str(file_path))
+
+    assert "LABORATORY TEST LIST" in pdf_text
+    assert "TEST NAME" in pdf_text
